7cad13935c016e0a9d4761fbda2f6f15f5c0e4ff
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-09-22 14:41:55 -0700

    super basic header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,2 +1,15 @@
 import React from 'react';
 import './header.styles.scss';
+
+export const Header = () => (
+  <div className='header'>
+    <section className='logo-container'>LOGO</section>
+    <section className='contact-options'>
+      <div>STUFF HERE SOON</div>
+      <div>STUFF HERE SOON ALSO</div>
+    </section>
+    <section className='hamburger'>hamburger here</section>
+  </div>
+);
+
+export default Header;
